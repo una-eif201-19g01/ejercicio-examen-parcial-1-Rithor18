8d21df3f95a3d32405698b03d39c96c7ed3a6ef6
Ricardo Thomas Rodriguez <richithomasft@gmail.com>
2019-09-06 08:47:12 -0600

    Añadidos metodos Get y Set

--- Cama.cpp
+++ Cama.cpp
@@ -3,4 +3,9 @@
 //
 
 #include "Cama.h"
+#include <ostream>
 
+Cama() {
+	setCodigo(´ ´);
+	setEstado()
+}
--- Pabellon.cpp
+++ Pabellon.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "Pabellon.h"
+#include <ostream>
+
+Pabellon() {
+	setId(´ ´);
+	setGenero(´ ´);
+
+}
+
+Pabellon::Pabellon(char id, char genero, int tamaño, int cantidad) : id(id), genero(genero), tamañ(tamaño) cantidad(cantidad) {
+
+}
+
+Pabellon::Pabellon(int tamano) {
+	for (int pabellon = 0; pabellon < tamaño; pabellon++) {
+
+	}
+}
+	//Metodos set
+void Pabellon::setId(char id){
+	id = id;
+}
+void Pabellon::setGenero(char genero) {
+	genero = genero;
+}
+void Pabellon::setTamano(int tamano) {
+	tamano = tamano;
+}
+void Pabellon::setCantidad(int cantidad) {
+
+}
+	//Metodos get
+char Pabellon::getId() const {
+	return id;
+}
+char Pabellon::getGenero() const {
+	return genero;
+}
+int Pabellon::getTamano()const {
+	return tamano;
+}
+int Pabellon::getCantidad()const {
+	return cantidad;
+}
